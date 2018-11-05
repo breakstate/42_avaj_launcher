7b8bfdfe3d00d4323f73f5752d8fa6d9d907f4df
bmoodley <juganaught72@gmail.com>
2018-11-05 15:10:06 +0200

    need to copy tokens into an array or something

--- src/simulator/FileHandler.java
+++ src/simulator/FileHandler.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileHandler {
 
     private String file;
-    static int lines = 0;
+    int lines = 0;
     int run = 0;
+    ArrayList<String[]> craft = new ArrayList();
     String tokens[];
     private String line;
     String deliminator = " ";
@@ -22,14 +24,32 @@ public class FileHandler {
     public void lexer(String fileArg){
         file = fileArg;
 
-        try(BufferedReader br = new BufferedReader((new FileReader(file)))){
-            while ((line = br.readLine()) != null){
-                System.out.println(line);
-                tokens = line.split(deliminator);
-                for (String token: tokens){
-                    System.out.println(token);
+        try(BufferedReader br = new BufferedReader((new FileReader(file)))) {
+            while ((this.line = br.readLine()) != null){
+                this.lines++;
+                System.out.println("reading line: " + line); // debug
+                this.tokens = this.line.split(deliminator);
+                if (this.lines == 1 && this.tokens.length == 1) {
+                    this.run = Integer.parseInt(this.tokens[0]);
+                    System.out.println("run = " + this.run); // debug
+                    System.out.println("tokens length = " + this.tokens.length); // debug
+                } else {
+                    String news[] = new String[0];
+                    news = tokens;
+
+                    craft.add(new String[0]);
+                    for (String s: tokens) {
+                        for (int i = 0 ; i < token)
+                        craft.get(0)[0] = s;
+                    }
+
+                }
+                for (String token: this.tokens){
+                    System.out.println(token); // debug (prints tokens)
                 }
             }
+            System.out.println("craft contents = " + this.craft); // debug (prints tokens)
+
         }
         catch (FileNotFoundException e){
             System.out.println("file not found");
